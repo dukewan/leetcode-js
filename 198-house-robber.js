6575053fc335c4f511c4aa914a7b3aead07688b4
yizhaohe <heyizhaobupt@gmail.com>
2015-10-21 15:59:33 -0700

    198

--- 198-house-robber.js
+++ 198-house-robber.js
@@ -4,21 +4,21 @@
  */
 
 function rob(nums) {
-	var length = nums.length;
+    var length = nums.length;
 
-	if (length === 0) {
-		return 0;
-	} else if (length == 1) {
-		return nums[0];
-	}
+    if (length === 0) {
+        return 0;
+    } else if (length == 1) {
+        return nums[0];
+    }
 
-	var opt = [];
-	opt[0] = nums[0],
-	opt[1] = Math.max(nums[0], nums[1]);
+    var opt = [];
+    opt[0] = nums[0],
+    opt[1] = Math.max(nums[0], nums[1]);
 
-	for (var i = 2; i < length; i++) {
-		opt[i] = Math.max(nums[i] + opt[i - 2], opt[i - 1]);
-	}
+    for (var i = 2; i < length; i++) {
+        opt[i] = Math.max(nums[i] + opt[i - 2], opt[i - 1]);
+    }
 
-	return opt[length - 1];
+    return opt[length - 1];
 }
